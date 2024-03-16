cc94d2f828a7b4297c425730788e84c72722ec6d
Kapil Soni <zacksnyder7ks@gmail.com>
2024-03-16 21:40:41 +0530

    Idk

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,18 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { Configuration, OpenAIApi } from "openai";
+import Replicate from "replicate";
 
-dotenv.config();
+// import { Configuration, OpenAIApi } from "openai";
 
-const router = express.Router();
+// dotenv.config();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// const router = express.Router();
 
-const openai = new OpenAIApi(config);
+// const config = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
+
+// const openai = new OpenAIApi(config);
 
 router.route("/").get((req, res) => {
   res.status(200).json({ message: "Hello from DALL.E ROUTES" });
@@ -21,14 +23,25 @@ router.route("/").post(async (req, res) => {
     const { prompt } = req.body;
     console.log(prompt);
 
-    const response = await openai.createImage({
-      model: "dall-e-1",
-      prompt: prompt,
-      n: 1,
-      size: "1024x1024",
-    });
+    // const response = await openai.createImage({
+    //   model: "dall-e-1",
+    //   prompt: prompt,
+    //   n: 1,
+    //   size: "1024x1024",
+    // });
 
-    image_url = response.data.data[0].url;
+    // image_url = response.data.data[0].url;
+
+    const replicate = new Replicate();
+
+    const response = await replicate.run(
+      "stability-ai/stable-diffusion:ac732df83cea7fff18b8472768c88ad041fa750ff7682a21affe81863cbe77e4",
+      {
+        input: {
+          prompt: prompt,
+        },
+      }
+    );
 
     const image = response.data.data[0].b64_json;
 
@@ -38,5 +51,3 @@ router.route("/").post(async (req, res) => {
     res.status(500).json({ message: "Something went wrong" });
   }
 });
-
-export default router;
