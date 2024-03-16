eeb75c256473a398981ae1a7e42723fd64cc9ba1
Kapil Soni <zacksnyder7ks@gmail.com>
2024-03-16 21:16:44 +0530

    Full Application

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+
+import Bruh from "./Bruh";
+import Login from "./pages/Login";
+
+function App() {
+
+  return (
+    
+    <main className="app transition-all ease-in">
+      <Routes>
+        <Route path="/home" element={<Bruh />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/*" element={<Navigate to="/login" />} />
+        {/* Add other routes here */}
+      </Routes>
+    </main>
+  )
+}
+
+export default App;
