e04a8b240a84f6673d167994ea4fcdc3d5abcca4
root <root@sameervivobook>
2024-03-16 21:57:38 +0530

    fashion healper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,18 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Bruh from "./Bruh";
 import Login from "./pages/Login";
 
-function App() {
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { auth } from "./Firebase";
 
+
+function App() {
+  const [authUser, setAuthUser] = useState(null);
   return (
     
     <main className="app transition-all ease-in">
       <Routes>
+        
         <Route path="/home" element={<Bruh />} />
         <Route path="/login" element={<Login/>} />
         <Route path="/*" element={<Navigate to="/login" />} />
